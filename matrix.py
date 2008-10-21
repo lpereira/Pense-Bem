5ba4633c79874060e14991acd120cf4614031e89
Eduardo Habkost <ehabkost@raisama.net>
2008-10-21 10:47:35 -0200

    Checking for known patterns on matrix output
    Highlight differences.
    
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- matrix.py
+++ matrix.py
@@ -2,6 +2,26 @@
 
 import sys
 
+# functions checking for known patterns:
+# - will return -1 for "not applied"
+#   0 for "match", > 0 for non-match
+
+def check_shift(m, bs, q, a):
+	o = m.get( (bs-10,q+1) )
+	if not o:
+		return -1
+
+	if a == o:
+		return 0
+	else:
+		return 1
+
+# list of pattern-checking functions,
+# and highlight escape code
+checkers = [
+	(check_shift,'1'),
+]
+
 m = {}
 f = open('SAMPLES')
 
@@ -26,5 +46,17 @@ for b in range(1, maxbook+1):
 	bs = b*10+1
 	sys.stdout.write('%03d ' % (bs))
 	for q in range(1,31):
-		sys.stdout.write(m.get( (bs,q), ' '))
+		a = m.get( (bs,q), ' ')
+
+		# check for known patterns
+		if a != ' ':
+			for function,color in checkers:
+				r = function(m, bs, q, a)
+
+				# doesn't match pattern?
+				if r > 0:
+					a = "\x1b[%sm%s\x1b[0m" % (color, a)
+					break
+
+		sys.stdout.write(a)
 	sys.stdout.write('\n')
