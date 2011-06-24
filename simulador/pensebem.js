470d6029650d59a39c6a96bbf913ba2eff75a395
Leandro A. F. Pereira <leandro@hardinfo.org>
2011-06-24 18:39:53 -0300

    [Simulator] Add possibility to play sounds. Audio code borrowed from http://code.google.com/p/felipesanches/source/browse/trunk/SVG/minigames/TuxVsMSNbug/minigame.js

--- simulador/pensebem.js
+++ simulador/pensebem.js
@@ -9,40 +9,140 @@ SigaMe = Dummy;
 NumeroDoMeio = Dummy;
 Operacao = Dummy;
 
+Som = {
+    encodeBase64: function(str) {
+        var out, i, len;
+        var c1, c2, c3;
+        const base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+        len = str.length;
+        i = 0;
+        out = "";
+        while(i < len) {
+            c1 = str.charCodeAt(i++) & 0xff;
+            if(i == len) {
+                out += base64EncodeChars.charAt(c1 >> 2);
+                out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+                out += "==";
+                break;
+            }
+            c2 = str.charCodeAt(i++);
+            if(i == len) {
+                out += base64EncodeChars.charAt(c1 >> 2);
+                out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+                out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+                out += "=";
+                break;
+            }
+            c3 = str.charCodeAt(i++);
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+            out += base64EncodeChars.charAt(c3 & 0x3F);
+        }
+        return out;
+    },
+    SampleRate: 44100,
+    encode8BitAudio: function(data) {
+		var n = data.length;
+		var integer = 0, i;
+
+		// 8-bit mono WAVE header template
+		var header = "RIFF<##>WAVEfmt \x10\x00\x00\x00\x01\x00\x01\x00<##><##>\x01\x00\x08\x00data<##>";
+
+		// Helper to insert a 32-bit little endian int.
+		function insertLong(value) {
+			var bytes = "";
+			for (i = 0; i < 4; ++i) {
+				bytes += String.fromCharCode(value % 256);
+				value = Math.floor(value / 256);
+			}
+			header = header.replace('<##>', bytes);
+		}
+
+		insertLong(36 + n); // chunk size
+		insertLong(Som.SampleRate); // sample rate
+		insertLong(Som.SampleRate); // byte rate
+		insertLong(n); // subchunk2 size
+
+		// Output sound data
+		for (var i = 0; i < n; ++i) {
+			header += String.fromCharCode(data[i] * 255);
+		}
+
+		return 'data:audio/wav;base64,' + Som.encodeBase64(header);
+    },
+    newTone: function (n) {
+        var audio = new Audio();
+        var samples = [];
+        const sampleRateBy8 = Som.SampleRate / 7;
+        const nBy50 = n * 50;
+        const nBy25 = n * 25;
+        for (var i = 0; i < sampleRateBy8; i++){
+            samples.push(i % nBy50 <= nBy25);
+        }
+
+        audio.setAttribute("src", Som.encode8BitAudio(samples));
+
+        audio.load();
+        audio.autoplay = false;
+        return function() {audio.play();};
+    },
+    playNote: function (n) {
+        const noteToToneTable = {
+            "c": Som.newTone(1),
+            "d": Som.newTone(2),
+            "e": Som.newTone(3),
+            "f": Som.newTone(4),
+            "g": Som.newTone(5),
+            "a": Som.newTone(6),
+            "b": Som.newTone(7),
+            "C": Som.newTone(8),
+            "D": Som.newTone(9)
+        };
+        var tone = noteToToneTable[n];
+        if (tone === undefined) {
+            return;
+        }
+        tone();
+    }
+};
+
 MemoriaSons = {
-	reset: function() {
-		PB.setDisplay("");
-		MemoriaSons.playQueue = [];
-	},
-	oneLoopIteration: function() {},
-	playAndClearQueue: function() {
-		for (var noteIndex in MemoriaSons.playQueue) {
-			MemoriaSons.playNote(MemoriaSons.playQueue[noteIndex]);
-		}
-		MemoriaSons.reset();
-	},
-	playNote: function(n) {
-		PB.setDisplay(n);
-	},
-	buttonPress: function(b) {
-		if (b == 'ENTER') {
-			MemoriaSons.playAndClearQueue();
-			return;
-		}
-		const buttonToNoteTable = {
-			"0": "p", "1": "c", "2": "d", "3": "e",
-			"4": "f", "5": "g", "6": "a", "7": "b",
-			"8": "C", "9": "D"
-		};
-		var note = buttonToNoteTable[b];
-		if (note === undefined) {
-			PB.beep();
-			return;
-		}
-		MemoriaSons.playQueue.push(note);
-		MemoriaSons.playNote(note);
-	},
-	buttonRelease: function(b) {}
+    reset: function() {
+        PB.setDisplay("");
+        MemoriaSons.playQueue = [];
+    },
+    oneLoopIteration: function() {},
+    playAndClearQueue: function() {
+        for (var noteIndex in MemoriaSons.playQueue) {
+            MemoriaSons.playNote(MemoriaSons.playQueue[noteIndex]);
+        }
+        MemoriaSons.reset();
+    },
+    playNote: function(n) {
+        Som.playNote(n);
+        PB.setDisplay(n);
+    },
+    buttonPress: function(b) {
+        if (b == 'ENTER') {
+            MemoriaSons.playAndClearQueue();
+            return;
+        }
+        const buttonToNoteTable = {
+            "0": "p", "1": "c", "2": "d", "3": "e",
+            "4": "f", "5": "g", "6": "a", "7": "b",
+            "8": "C", "9": "D"
+        };
+        var note = buttonToNoteTable[b];
+        if (note === undefined) {
+            PB.beep();
+            return;
+        }
+        MemoriaSons.playQueue.push(note);
+        MemoriaSons.playNote(note);
+    },
+    buttonRelease: function(b) {}
 };
 
 Aritmetica = {
