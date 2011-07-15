07762c13abf0749efd8054d4563d84468d75d60d
Thiago Alvarenga Lechuga <thiagoalz@gmail.com>
2011-07-16 05:51:55 +0800

    Changing book answer algorithm to answer questions > 16. Need to test it more and then move it to javascript. Now I have: Correct answers: 701. Unknown answers: 5. 175 ERRORS. :(.

--- calcanswer.py
+++ calcanswer.py
@@ -3,13 +3,25 @@
 import samples
 
 def answer(book,question):
-	if question <= 15:
-		pattern = "cddbaadcbdaadcbb"
-		return pattern[(book+question)%16]
+	pattern_list = ["cddbaadcbdaadcbb",
+		 "ccbdddbdababdaca",
+		 "ddcbdababdacaccb",
+		 "aabbcdcdddacaabc",
+		 "cXabadbbbcdcddda", # Position [1] is unknown. More info needed!
+		 "aXcbadbadbbddccb", # Position [1] is unknown. More info needed!
+		 "aXdbadbbdcccbadb", # Position [1] is unknown. More info needed!
+		 "dXbbdabbdabdabcc", # Position [1] is unknown. More info needed!
+		 "bXdccddaacdbbddb", # Position [1] is unknown. More info needed!
+		 "cXcdcbbbdabdddcd"] # Position [1] is unknown. More info needed!
+
+	pattern_number = (question - 1) / 15 # For each 15 questions the pattern changes (1-15 != 16-30 != ...)
+	question = (question % 15) if (question % 15) > 0 else 15 # Need question numbers ranging from 1 to 15
+
+	pattern = pattern_list[pattern_number]
+	candidate = pattern[(book+question)%16]
+
+	return candidate if candidate != "X" else None
 	
-	return None
-
-
 correct = 0
 unknown = 0
 errors = 0
